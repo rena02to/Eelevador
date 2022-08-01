e12e548896c4358c81a8b5686ff4d146ab865bb6
rena02to <108096649+rena02to@users.noreply.github.com>
2022-07-31 21:42:54 -0300

    Update main.py

--- main.py
+++ main.py
@@ -95,7 +95,7 @@ def main():
     #tuneis descendo
     requisicoes_tunel3 = []
     
-    #andares atuais do elevador
+    #andares atuais dos elevadores principais
     elevador_c = -2
     elevador_b = -1
     elevador_a = 0
@@ -103,61 +103,135 @@ def main():
     elevador_e = 301
     elevador_f = 303
     
+    #andares atuais dos elevadores secundarios
+    elevador_g = -2
+    elevador_h = -1
+    elevador_i = 0
+    elevador_j = 301
+    elevador_k = 301
+    elevador_l = 303
+    
     #contadores de energia e tempo de espera
     cont_energ_a = 0
-    cont_energ_d = 0
+    cont_energ_b = 0
+    cont_energ_c = 0
     cont_t_a = 0
-    cont_t_d = 0
+    cont_t_b = 0
+    cont_t_c = 0
     
     contador = 0
     contador2 = 0
     
     #primeira entrada
     requisicoes = organiza_req(requisicoes_tunel1, requisicoes_tunel3)
-    while requisicoes != None:
-        if requisition() == True:
-            requisicoes = None
-            requisicoes = organiza_req(requisicoes_tunel1, requisicoes_tunel3)
-        if requisicoes[0] != []:
-            contador = 0
+    if requisicoes[0] != []:
+        while requisicoes != ([], []):
+            if requisition() == True:
+                requisicoes = None
+                requisicoes = organiza_req(requisicoes_tunel1, requisicoes_tunel3)
+            
+            #subindo
             os.system("cls")
-            for teste in requisicoes[0]:
-                if teste[0] == elevador_a - 1:
-                    requisicoes[0][contador] = (requisicoes[0][contador][0] + 1, requisicoes[0][contador][1])
-                contador = contador + 1
+            if elevador_b < 0:
+                #somente elevador a
+                print("Elevador A atendendo as requisicoes!")
+                for auxiliar in requisicoes[0]:
+                    if auxiliar[0] == elevador_a and teste[0] != teste[1]:
+                        print("Porta aberta(elevador A)!")
+                
+                if requisicoes[0] != []:
+                    #elevador a, b e c
+                    contador = 0
+                    for teste in requisicoes[0]:
+                        if teste[0] == elevador_a - 1:
+                            requisicoes[0][contador] = (requisicoes[0][contador][0] + 1, requisicoes[0][contador][1])
+                        contador = contador + 1    
+                        
+                    for teste in requisicoes[0]:
+                        if teste[0] == teste[1]:
+                            print("Porta aberta(elevador A)!")
+                            requisicoes[0].pop()
+            else:
+                #elevador a e b
+                if elevador_c > -1:
+                    print("Elevador A, elevador B e elevador C atendendo as requisicoes!")
+                else:
+                    print("Elevador a e elevador B atendendo as requisicoes!")
+                    for auxiliar in requisicoes[0]:
+                        if auxiliar[0] == elevador_a and auxiliar[0] != auxiliar[1]:
+                            print("Porta aberta(elevador A)!")
+                        if auxiliar[0] == elevador_b and auxiliar[0] != auxiliar[1]:
+                            print("Porta aberta(elevador B)!")
+                    
+                    if requisicoes[0] != []:
+                        contador = 0
+                        for teste in requisicoes[0]:
+                            if teste[0] == elevador_a - 1:
+                                requisicoes[0][contador] = (requisicoes[0][contador][0] + 1, requisicoes[0][contador][1])
+                            if teste[0] == elevador_b - 1:
+                                requisicoes[0][contador] = (requisicoes[0][contador][0] + 1, requisicoes[0][contador][1])
+                            contador = contador + 1
+                            
+                        for teste in requisicoes[0]:
+                            if teste[0] == teste[1]:
+                                requisicoes[0].pop()
+                                if elevador_a == teste[0]:
+                                    print("Porta aberta(elevador A)!")
+                                    elevador_a = 0
+                                if elevador_b == teste[0]:
+                                    print("Porta aberta(elevador B)!")
+                                    elevador_c = -1
+                                if elevador_c == teste[0]:
+                                    print("Porta aberta(elevador C)!")
+                                    elevador_c = - 2
             
-            for teste in requisicoes[0]:
-                if teste[0] == teste[1]:
-                    print("Porta aberta!")
-                    requisicoes[0].pop()
-        
-        if requisicoes[1] != []:
-            contador = 0
-            os.system("cls")
-            for teste in requisicoes[1]:
-                if teste[0] == elevador_d + 1:
-                    requisicoes[1][contador] = (requisicoes[1][contador][0] - 1, requisicoes[0][contador][1])
-                contador = contador + 1
             
-            for teste in requisicoes[1]:
-                if teste[0] == teste[1]:
-                    print("Porta aberta!")
-                    requisicoes[0].pop()
-         
-        elevador_a = elevador_a + 1   
-        elevador_d = elevador_d - 1
-        cont_energ_a = cont_energ_a + 1
-        cont_energ_d = cont_energ_d + 1
-        cont_t_a = cont_t_a + 1
-        cont_t_d = cont_t_d + 1
-        print(f"Gasto energetico de A: {cont_energ_a}\nGasto energetico D: {cont_energ_d}\nTempo de A: {cont_t_a}\nTempo de D: {cont_t_d}\nAndar atual do elevador A: {elevador_a}\nAndar atual do elevador D: {elevador_d}\nGasto total de energia: {cont_energ_a + cont_energ_d}")
-        contador2 = contador2 + 1
-        
-        print(requisicoes)
+            #print(f"{requisicoes}\n")
             
-        if requisicoes == ([], []):
-            print("Requisicoes atendidas!")
-            break
+            contador2 = 0
+            for assistant in requisicoes[0]:
+                #print(f"Gasto energetico de A: {cont_energ_a}\nTempo de A: {cont_t_a}\nAndar atual do elevador A: {elevador_a}")
+                if assistant[0] < elevador_a:
+                    if elevador_b <= assistant[0]:
+                        print(f"Gasto energetico de A: {cont_energ_a}\nTempo de A: {cont_t_a}\nAndar atual do elevador A: {elevador_a}")
+                        print(f"Gasto energetico de B: {cont_energ_b}\nTempo de B: {cont_t_b}\nAndar atual do elevador B: {elevador_b}")
+                        elevador_a = elevador_a + 1
+                        cont_energ_a = cont_energ_a + 1
+                        contador2 = contador2 + 1
+                        cont_t_a = cont_t_a + 1
+                        
+                        elevador_b = elevador_b + 1
+                        cont_energ_b = cont_energ_b + 1
+                        cont_t_b = cont_t_b + 1
+                    else:
+                        print(f"Gasto energetico de A: {cont_energ_a}\nTempo de A: {cont_t_a}\nAndar atual do elevador A: {elevador_a}")
+                        print(f"Gasto energetico de B: {cont_energ_b}\nTempo de B: {cont_t_b}\nAndar atual do elevador B: {elevador_b}")
+                        print(f"Gasto energetico de C: {cont_energ_c}\nTempo de C: {cont_t_c}\nAndar atual do elevador C: {elevador_c}")
+                        elevador_a = elevador_a + 1
+                        cont_energ_a = cont_energ_a + 1
+                        contador2 = contador2 + 1
+                        cont_t_a = cont_t_a + 1
+                        
+                        elevador_b = elevador_b + 1
+                        cont_energ_b = cont_energ_b + 1
+                        cont_t_b = cont_t_b + 1
+                        
+                        elevador_c = elevador_c + 1
+                        cont_energ_c = cont_energ_c + 1
+                        cont_t_c = cont_t_c + 1
+                else:
+                    if contador2 == len(requisicoes[0]) - 1:
+                        print(f"Gasto energetico de A: {cont_energ_a}\nTempo de A: {cont_t_a}\nAndar atual do elevador A: {elevador_a}")
+                        elevador_a = elevador_a + 1
+                        cont_energ_a = cont_energ_a + 1
+                        contador2 = contador2 + 1
+                        cont_t_a = cont_t_a + 1
+                contador2 = contador2 + 1
+            
+            if requisicoes[0] == []:
+                print("Requisicoes atendidas!")
+                break
+    
     elevador_a = 0
 
 main()
